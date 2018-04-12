2a47b40df9b66db4623419ca6678b5930fbda96f
TerryGarcia <anonymous@Desktop.localdomain>
2018-04-11 21:39:24 -0500

    Changed polybar again

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,5 +1,11 @@
+#!/bin/bash
+
 killall -q polybar
 
 while pgrep -u $UID -x polybar>/dev/null; do sleep 1; done
 
-polybar mainBar &
+if [ "$HOSTNAME" == "Desktop" ]; then
+    polybar desktop &
+else
+    polybar laptop &
+fi
