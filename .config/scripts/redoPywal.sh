724b9bf35d93120f89d804ce531cf759d614f65e
mysteriousLynx <mysteriousLynx2011@gmail.com>
2018-05-21 21:04:59 -0500

    New changes to colors using pywal and added dunst

---
+++ .config/scripts/redoPywal.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+source "${HOME}/.cache/wal/colors.sh"
+
+wal -i ~/Pictures/Wallpapers
+~/.config/scripts/updateDunstrc.sh
+~/.config/scripts/reloadDunst.sh
+bspc config normal_border_color "$color1"
+bspc config active_border_color "$color2"
+bspc config focused_border_color "$color15"
+bspc config presel_border_color "$color1"
