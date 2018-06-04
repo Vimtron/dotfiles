caec57769d42acd64cda6ba489c365baec7d2318
mysteriousLynx <mysteriousLynx2011@gmail.com>
2018-06-03 23:48:08 -0500

    Fixed toggleSystray.sh

--- .config/scripts/toggleSystray.sh
+++ .config/scripts/toggleSystray.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 file=$(dirname "$(readlink -f "$0")")/SYSTRAY_ACTIVE
+touch $file
 
-if [ ! -w file -o ! -s file -o $(cat file) = "false" ]; then
-    echo true > file
+if [[ ! -w file ||  ! -s file || $(cat $file) = "false" ]]; then
+    echo true > $file
 else
-    echo false > file
+    echo false > $file
 fi
 
-~/.config/polybar/launch.sh $(cat file)
+~/.config/polybar/launch.sh $(cat $file)
